ec37bbca6d5ca5a42b4287cceb1decbe414a103a
Anatol Belski <ab@php.net>
2017-08-22 13:48:15 +0200

    Improve Mediawiki URL generation

--- pgo/cases/mediawiki/TrainingCaseHandler.php
+++ pgo/cases/mediawiki/TrainingCaseHandler.php
@@ -98,6 +98,11 @@ class TrainingCaseHandler extends Abstracts\TrainingCase implements Interfaces\T
 		if (preg_match_all(", href=\"([^\"]+)\",", $s, $m)) {
 			foreach ($m[1] as $u) {
 				if ("/" == $u[0] && "/" != $u[1] && !in_array(substr($u, -3), array("css", "xml", "ico"))) {
+					$u = str_replace(
+						array("&amp;"),
+						array("&"),
+						$u
+					);
 					$ur = "http://" . $this->getHttpHost() . ":" . $this->getHttpPort() . $u;
 					if (!in_array($ur, $lst)) {
 						$lst[] = $ur;
